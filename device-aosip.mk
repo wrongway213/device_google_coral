78e4739cc48818f38edc413750aae633920710f3
Ethan Chen <intervigil@gmail.com>
2018-12-01 13:53:00 -0800

    coral: Declare EUICC permission
    Change-Id: I1b151021ca9fc6cf35141b2ccb337bc83197089e

--- device-aosip.mk
+++ device-aosip.mk
@@ -14,5 +14,9 @@
 # limitations under the License.
 #
 
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-aosip
