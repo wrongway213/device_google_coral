4a22b163b88ffae0fffefcaee3e0b800ed0cbe76
Ethan Chen <intervigil@gmail.com>
2018-12-01 13:53:00 -0800

    floral: Declare EUICC permission
    Change-Id: I1b151021ca9fc6cf35141b2ccb337bc83197089e

--- device-aosip.mk
+++ device-aosip.mk
@@ -14,6 +14,10 @@
 # limitations under the License.
 #
 
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-aosip
 
