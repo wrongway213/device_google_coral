18bd3fc217ca76393230f1bcfa2cde9832ae4cb7
freak07 <michalechner92@googlemail.com>
2019-03-12 09:46:52 +0100

    coral: enable usage of psi monitor
    Change-Id: I327fb90070bf1bc0bef09e7ce9831487aa95de69

--- device-common.mk
+++ device-common.mk
@@ -34,6 +34,10 @@ PRODUCT_PROPERTY_OVERRIDES += vendor.audio.offload.buffer.size.kb=256
 PRODUCT_PACKAGES += \
     SystemUIGoogle
 
+# LMK
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lmk.use_psi=true
+
 # Enable AAudio MMAP/NOIRQ data path.
 # 1 is AAUDIO_POLICY_NEVER  means only use Legacy path.
 # 2 is AAUDIO_POLICY_AUTO   means try MMAP then fallback to Legacy path.
