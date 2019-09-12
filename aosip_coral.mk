7782aca435505d1b6646fd635edcbf7ade88b87c
LuK1337 <priv.luk@gmail.com>
2019-09-12 23:57:18 +0200

    coral: Override PRODUCT_DEVICE instead of TARGET_DEVICE
    * TARGET_DEVICE isn't really used for generating props
      anymore as tools/buildinfo_common.sh is using
      PRODUCT_DEVICE instead.
    
    Change-Id: I72ab40e3ebc63b3acbcf00524227189d31207fa6

--- aosip_coral.mk
+++ aosip_coral.mk
@@ -39,7 +39,7 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="coral" \
-    TARGET_DEVICE="coral" \
+    PRODUCT_DEVICE="coral" \
     PRIVATE_BUILD_DESC="coral-user 10 QQ2A.200501.001.B2 6352890 release-keys"
 
 BUILD_FINGERPRINT := "google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys"
