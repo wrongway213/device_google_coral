79b73cfcfa12b81e3d0eeade6cf29ed132e29892
LuK1337 <priv.luk@gmail.com>
2019-11-22 15:35:26 +0100

    floral: Exclude Snap overlays from RRO
    Change-Id: Ibefb7acaa158eea787618d1696057b75d2541e87

--- device-aosip.mk
+++ device-aosip.mk
@@ -24,6 +24,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-aosip
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap \
 
 # Partitions
 AB_OTA_PARTITIONS += \
