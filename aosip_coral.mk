547b224da1962f076a8f1d8304f2285fec918338
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2019-12-14 11:02:23 -0500

    AOSiP-ify
    Change-Id: I27998744b28905e174f5e35d77cbbd92d759e05c
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

---
+++ aosip_coral.mk
@@ -0,0 +1,48 @@
+# Copyright (C) 2020 AOSiP
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file is the build configuration for an aosp Android
+# build for marlin hardware. This cleanly combines a set of
+# device-specific aspects (drivers) with a device-agnostic
+# product configuration (apps). Except for a few implementation
+# details, it only fundamentally contains two inherit-product
+# lines, aosp and du, hence its name.
+#
+
+# Include AOSiP common configuration
+include vendor/aosip/config/common_full_phone.mk
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/google/coral/aosp_coral.mk)
+-include device/google/coral/device-aosip.mk
+
+PRODUCT_NAME := aosip_coral
+PRODUCT_DEVICE := coral
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 4 XL
+PRODUCT_MANUFACTURER := Google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="coral" \
+    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001 6578210 release-keys"
+
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
+
+BOARD_PREBUILT_VENDORIMAGE := vendor/images/coral/vendor.img
+
+$(call inherit-product-if-exists, vendor/google/coral/coral-vendor.mk)
