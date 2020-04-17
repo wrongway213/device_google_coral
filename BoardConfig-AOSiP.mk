54944637d6fc4b82ce86d5da3f083494369841bf
Chirayu Desai <chirayudesai1@gmail.com>
2020-04-17 19:30:31 +0530

    floral: Inline kernel building
    Change-Id: I1cc3ab1ff45a18d686107a7495b80777b769820f

--- BoardConfig-AOSiP.mk
+++ BoardConfig-AOSiP.mk
@@ -18,4 +18,14 @@
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
 
+# Kernel
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
+KERNEL_LD := LD=ld.lld
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := floral_defconfig
+TARGET_KERNEL_SOURCE := kernel/google/floral
+TARGET_NEEDS_DTBOIMAGE := true
+
 -include vendor/google/coral/BoardConfigVendor.mk
