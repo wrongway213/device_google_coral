d2363f9d2ecfc264801af51e3f9e61ff841f449c
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-07-22 22:24:39 -0400

    coral: set LOCAL_PATH for device-aosip
    fixes call when building flame
    
    Change-Id: I80920cc58efc2078cfef1100953e922a19d4c80f

--- device-aosip.mk
+++ device-aosip.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+LOCAL_PATH := device/google/coral
+
 # EUICC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.euicc.xml:system/etc/permissions/android.hardware.telephony.euicc.xml
