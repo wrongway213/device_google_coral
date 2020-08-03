52a9b6ad4be9e0b510e6c603ca9c1badc08d3c19
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-08-03 19:04:34 -0400

    coral: update fingerprint to QQ3A.200805.001/6578210
    Change-Id: Ib4f7970f7a08f05c8b59e9ef3e70e1a566682b0b

--- aosip_coral.mk
+++ aosip_coral.mk
@@ -41,12 +41,12 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="coral" \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200705.002 6506677 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001 6578210 release-keys"
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys
+    ro.build.fingerprint=google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/coral/coral-vendor.mk)
 $(call inherit-product-if-exists, vendor/google/psu/google-psu.mk)
