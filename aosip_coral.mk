92cb56e1aec732faf4d262a85cd16db3f7f0d69f
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2021-01-04 19:12:48 -0500

    floral: update fingerprints to RQ1A.210105.003
    Change-Id: Iacf6baadcf1a7b52d05b786753dc27dae3e9411c

--- aosip_coral.mk
+++ aosip_coral.mk
@@ -36,11 +36,11 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="coral" \
-    PRIVATE_BUILD_DESC="coral-user 11 RQ1A.201205.008 6943376 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 11 RQ1A.210105.003 7005429 release-keys"
 
-BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.201205.008/6943376:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/coral/coral:11/RQ1A.201205.008/6943376:user/release-keys
+    ro.build.fingerprint=google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/coral/coral-vendor.mk)
