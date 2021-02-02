f4a04acad1bf6222319c0a291076aafdd7929645
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2021-02-02 17:18:18 -0500

    floral: update fingerprints to RQ1A.210205.004
    Change-Id: Ie6ad58f6cc578ac2e24c45756a83f309b96ff790

--- aosip_coral.mk
+++ aosip_coral.mk
@@ -36,11 +36,11 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="coral" \
-    PRIVATE_BUILD_DESC="coral-user 11 RQ1A.210105.003 7005429 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 11 RQ1A.210205.004 7038034 release-keys"
 
-BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RQ1A.210205.004/7038034:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys
+    ro.build.fingerprint=google/coral/coral:11/RQ1A.210205.004/7038034:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/coral/coral-vendor.mk)
--- aosip_flame.mk
+++ aosip_flame.mk
@@ -36,11 +36,11 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="flame" \
-    PRIVATE_BUILD_DESC="flame-user 11 RQ1A.210105.003 7005429 release-keys"
+    PRIVATE_BUILD_DESC="flame-user 11 RQ1A.210205.004 7038034 release-keys"
 
-BUILD_FINGERPRINT := "google/flame/flame:11/RQ1A.210105.003/7005429:user/release-keys"
+BUILD_FINGERPRINT := "google/flame/flame:11/RQ1A.210205.004/7038034:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/flame/flame:11/RQ1A.210105.003/7005429:user/release-keys
+    ro.build.fingerprint=google/flame/flame:11/RQ1A.210205.004/7038034:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/flame/flame-vendor.mk)
